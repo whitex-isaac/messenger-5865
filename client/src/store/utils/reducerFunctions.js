f36131abccb1132cf41a0df5054b17b378307cda
whitex-isaac <whitex.isaac@gmail.com>
2021-11-25 00:44:06 -0600

    client files reverted to the starter code linting format

--- client/src/store/utils/reducerFunctions.js
+++ client/src/store/utils/reducerFunctions.js
@@ -1,88 +1,88 @@
 export const addMessageToStore = (state, payload) => {
-	const { message, sender } = payload;
+  const { message, sender } = payload;
 
-	// if sender isn't null, that means the message needs to be put in a brand new convo
-	if (sender !== null) {
-		const newConvo = {
-			id: message.conversationId,
-			otherUser: sender,
-			messages: [message],
-		};
-		newConvo.latestMessageText = message.text;
-		return [newConvo, ...state];
-	}
+  // if sender isn't null, that means the message needs to be put in a brand new convo
+  if (sender !== null) {
+    const newConvo = {
+      id: message.conversationId,
+      otherUser: sender,
+      messages: [message],
+    };
+    newConvo.latestMessageText = message.text;
+    return [newConvo, ...state];
+  }
 
-	return state.map((convo) => {
-		if (convo.id === message.conversationId) {
-			const updatedConvo = {
-				...convo,
-				messages: [...convo.messages, message],
-				latestMessageText: message.text,
-			};
-			return updatedConvo;
-		} else {
-			return convo;
-		}
-	});
+  return state.map((convo) => {
+    if (convo.id === message.conversationId) {
+      const updatedConvo = {
+        ...convo,
+        messages: [...convo.messages, message],
+        latestMessageText: message.text,
+      };
+      return updatedConvo;
+    } else {
+      return convo;
+    }
+  });
 };
 
 export const addOnlineUserToStore = (state, id) => {
-	return state.map((convo) => {
-		if (convo.otherUser.id === id) {
-			const convoCopy = { ...convo };
-			convoCopy.otherUser.online = true;
-			return convoCopy;
-		} else {
-			return convo;
-		}
-	});
+  return state.map((convo) => {
+    if (convo.otherUser.id === id) {
+      const convoCopy = { ...convo };
+      convoCopy.otherUser.online = true;
+      return convoCopy;
+    } else {
+      return convo;
+    }
+  });
 };
 
 export const removeOfflineUserFromStore = (state, id) => {
-	return state.map((convo) => {
-		if (convo.otherUser.id === id) {
-			const convoCopy = { ...convo };
-			convoCopy.otherUser.online = false;
-			return convoCopy;
-		} else {
-			return convo;
-		}
-	});
+  return state.map((convo) => {
+    if (convo.otherUser.id === id) {
+      const convoCopy = { ...convo };
+      convoCopy.otherUser.online = false;
+      return convoCopy;
+    } else {
+      return convo;
+    }
+  });
 };
 
 export const addSearchedUsersToStore = (state, users) => {
-	const currentUsers = {};
+  const currentUsers = {};
 
-	// make table of current users so we can lookup faster
-	state.forEach((convo) => {
-		currentUsers[convo.otherUser.id] = true;
-	});
+  // make table of current users so we can lookup faster
+  state.forEach((convo) => {
+    currentUsers[convo.otherUser.id] = true;
+  });
 
-	const newState = [...state];
-	users.forEach((user) => {
-		// only create a fake convo if we don't already have a convo with this user
-		if (!currentUsers[user.id]) {
-			let fakeConvo = { otherUser: user, messages: [] };
-			newState.push(fakeConvo);
-		}
-	});
+  const newState = [...state];
+  users.forEach((user) => {
+    // only create a fake convo if we don't already have a convo with this user
+    if (!currentUsers[user.id]) {
+      let fakeConvo = { otherUser: user, messages: [] };
+      newState.push(fakeConvo);
+    }
+  });
 
-	return newState;
+  return newState;
 };
 
 export const addNewConvoToStore = (state, recipientId, message) => {
-	return state.map((convo) => {
-		if (convo.otherUser.id === recipientId) {
-			const newConvo = {
-				...convo,
-				id: message.conversationId,
-				messages: [...convo.messages, message],
-				latestMessageText: message.text,
-			};
+  return state.map((convo) => {
+    if (convo.otherUser.id === recipientId) {
+      const newConvo = {
+        ...convo,
+        id: message.conversationId,
+        messages: [...convo.messages, message],
+        latestMessageText: message.text,
+      };
 
-			return newConvo;
-		} else {
-			return convo;
-		}
-	});
+      return newConvo;
+    } else {
+      return convo;
+    }
+  });
 };
